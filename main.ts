58713b12226500a5d016283b321574bc103c9c9e
izaskunbraualdave <93262422+izaskunbraualdave@users.noreply.github.com>
2022-03-23 14:11:44 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 50)
+    basic.pause(2800)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
+    basic.pause(2800)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    basic.pause(500)
+})
+DFRobotMaqueenPlus.I2CInit()
